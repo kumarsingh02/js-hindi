0d84d7f35da0b3fc43d670ecdbbc53f52ab131dd
kumarsingh02 <satish.singh@ubione.in>
2024-05-31 09:41:23 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alret ( 3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3); // code readablity shoud be high 
+
+    console.log(" Ubi-Fi");
+
+
+    let name = " Ubi-Fi"
+    let age = 18 
+    let isLoggedIn = false
+
+
+    // nuber => 2 to power 53
+    // binint 
+    // string => ""
+    // boolean => true / false
+    // null => standalone value
+    // undefined => 
+    // symbol => unique
+
+
+
+
+    
