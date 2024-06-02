cc577f5d2f833abaa097e0e918d6ac6126396f2c
kumarsingh02 <satish.singh@ubione.in>
2024-06-02 13:06:15 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valuInNumber = Number(score)
+console.log(typeof valuInNumber);
+
+let isLoggedIn = true
+
+//console.log(isLoggedIn);
+
+let booleanIsLoggedin = 1 
+
+//console.log(booleanIsLoggedin);
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
