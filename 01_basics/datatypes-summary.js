56612bc66c123ec3c96ffe3e390f8bc419fa449e
kumarsingh02 <satish.singh@ubione.in>
2024-06-03 11:11:49 +0000

    update

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,12 @@
+// Primitive
+
+// 7 types : String , Number , Boolean , Null , Undified , symbol , BigInt
+
+const score = 100 
+
+
+
+
+// Reference  ( Non Primitive)
+
+// Array, Objests, Functions 
